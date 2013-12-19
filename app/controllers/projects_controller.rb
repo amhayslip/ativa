98b0f135f2328437f8c18eb2247ba7d4497146f0
Ben White <ben.white@utexas.edu>
2013-12-19 14:47:32 -0600

    Prevent entire page from rendering upon Ajax request

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -32,6 +32,7 @@ class ProjectsController < ApplicationController
 
     respond_to do |format|
       if @project.save
+        format.js { render layout: false }
         format.html { redirect_to @project, notice: 'Project was successfully created.' }
         format.json { render action: 'show', status: :created, location: @project }
       else
